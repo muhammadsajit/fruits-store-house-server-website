21b7b8a35f2117ddd8664f30b2e09731acf39ca4
muhammadsajit <sajit.du.niter@gmail.com>
2022-05-07 20:23:35 +0600

    single item show api added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express =require('express');
 const app = express();
 const cors =require('cors');
 const port = process.env.PORT ||5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 //use middleware
@@ -23,6 +23,12 @@ async function run(){
             const items = await cursor.toArray();
             res.send(items)
           });
+          app.get('/inventory/:id',async(req,res)=>{
+             const id=req.params.id ;
+             const query={_id:ObjectId(id)};
+             const inventory = await fruitsCollection.findOne(query);
+             res.send(inventory);
+          })
 
 
 
