8a49d69d2056b2b590c548b1a7cfe015d5337a6f
muhammadsajit <sajit.du.niter@gmail.com>
2022-05-09 15:26:48 +0600

    delete api added

--- index.js
+++ index.js
@@ -54,7 +54,15 @@ async function run(){
               const result= await fruitsCollection.insertOne(newItem);
               res.send(result)
 
-          })
+          });
+          //delete
+          app.delete('/inventory/:id',async(req,res)=>{
+              const id= req.params.id;
+              const query ={_id:ObjectId(id)};
+              const result = await fruitsCollection.deleteOne(query);
+              res.send(result);
+          });
+
 
 
 
